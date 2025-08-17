80cb6e4318b6285004b64487366ec16d0f11f40e
Bahar-sidrap <pkmlawawoi23@gmail.com>
2025-08-17 14:09:49 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "imunisasi-tt-cache-v1";
+const CACHE_NAME = "imunisasi-tt-cache-v2";
 const ASSETS_TO_CACHE = [
   "./",
   "./index.html",
@@ -37,3 +37,4 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
